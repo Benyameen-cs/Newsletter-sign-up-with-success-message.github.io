0b204b8c816e18bde2e2eca4b2b5bbeed77a80ef
Benyameen-cs <benyameen009@gmail.com>
2025-04-20 09:41:17 +0500

    the working of confirmation messege is done and now making it responsive for mobile design

--- NewsletterLogic.js
+++ NewsletterLogic.js
@@ -1,11 +1,14 @@
-
-const emailPattern = /^[^\s@#\$]+@[^\s@#\$]+\.[^\s@#\$]+$/;
 let emailValue = "";
+const emailPattern = /^[^\s@#\$]+@[^\s@#\$]+\.[^\s@#\$]+$/;
 const emailInput = document.querySelector("#emailInput");
+let newsletterContainer = document.querySelector("#newsletter-container");
+let newsletterSuccessContainer = document.querySelector("#newsletter-success-container")
 let newsletterFormBtn = document.querySelector("#newsletterFormBtn");
+let dismissMessegeBtn = document.querySelector("#dismiss-message");
+let tab = false;
 
 emailInput.addEventListener("input", (e) => {
-      
+    console.log(e.target.value)
     
     let emailErrorMsg = document.querySelector("#emailErrorMsg");
 
@@ -20,7 +23,7 @@ emailInput.addEventListener("input", (e) => {
     else if (e.target.value === "") {
         emailInput.classList.add("emailInputState-1");
         emailErrorMsg.classList.add("emailErrorState-1");  
-        alert("empty field email required");
+        
     }
 
     else {
@@ -30,12 +33,27 @@ emailInput.addEventListener("input", (e) => {
         emailInput.classList.remove("emailInputState-2");
         emailErrorMsg.classList.remove("emailErrorState-2");
         emailValue = e.target.value
+        tab = true;
         newsletterFormBtn.disabled = false;
     }
 })
 
 newsletterFormBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log(emailValue)    
+    if (tab) {
+        document.querySelector("#showEmail").innerText = emailValue;
+        newsletterContainer.classList.add("newsletter-container-hide");
+        newsletterSuccessContainer.classList.remove("newsletter-success-container-hide")
+        tab = true;
+    }
+    emailInput.value = "";
+    emailValue = "";
+    tab = false;
+
+})
+
+dismissMessegeBtn.addEventListener("click", () => {
+    newsletterContainer.classList.remove("newsletter-container-hide");
+    newsletterSuccessContainer.classList.add("newsletter-success-container-hide");
 })
 
